801977deabb2f8eea07475f466f1378aa8a4ac95
datng <datnguyennguyen.2000@gmail.com>
2020-10-11 23:40:07 +0700

    Commit lan 1

--- app/src/main/java/datnguyen/ute/profileproject/LoginActivity.java
+++ app/src/main/java/datnguyen/ute/profileproject/LoginActivity.java
@@ -2,13 +2,54 @@ package datnguyen.ute.profileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView txtSignUp;
+    EditText eUser, ePassword;
+    Button btnLogin;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtSignUp = (TextView) findViewById(R.id.txtSignUp);
+        eUser = (EditText) findViewById(R.id.eUser);
+        ePassword = (EditText) findViewById(R.id.ePassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenSignUpActi();
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenMainActivity();
+            }
+        });
+    }
+
+    public void OpenSignUpActi() {
+        Intent intent = new Intent(this,SignUpActivity.class);
+        startActivity(intent);
+    }
+    public void OpenMainActivity(){
+        if (eUser.getText().toString().equals("admin") && eUser.getText().toString().equals("admin")){
+            Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this,ProfileActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(LoginActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/datnguyen/ute/profileproject/MainActivity.java
+++ app/src/main/java/datnguyen/ute/profileproject/MainActivity.java
@@ -2,13 +2,23 @@ package datnguyen.ute.profileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int SPLASH_SCREEN = 5000;
+
+    Animation topAnimation, bottomAnimation;
+    ImageView imageView;
+    TextView content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        imageView = (ImageView) findViewById(R.id.icon_ute);
+        content = (TextView) findViewById(R.id.content);
+
+        imageView.setAnimation(topAnimation);
+        content.setAnimation(bottomAnimation);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dep = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(dep);
+                finish();
+            }
+        },2000);
     }
 }
